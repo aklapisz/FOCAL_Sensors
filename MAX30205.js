9c4f7bf817575d94a42286a2d639ae7a5aa4d9d7
aklapisz <35926117+aklapisz@users.noreply.github.com>
2020-08-07 03:08:39 +0000

    Update MAX30205.js

--- MAX30205.js
+++ MAX30205.js
@@ -1,3 +1,46 @@
+E.setFlags({pretokenise:1});
+
+//object that holds all relavant register addresses on the MAX30205
+const C = {
+  
+  //main address
+  I2C_ADDR: 0b1001000,
+  
+  //register address
+  REG_TEMPERATURE: 0x00, //temperature data is stored here
+  REG_CONFIGURATION: 0x01, //Set bit D0 to 1 to place the device in shutdown mode
+  //Bit D5 selects the temperature data format for the temperature, TOS, and THYST registers. When D5 is 0 (normal format), the data format is two’s complement with a range of 0 C to +50 C.
+  REG_T_HYST: 0x02, //temperature data is stored here
+  REG_T_OS: 0x03 //temperature data is stored here
+
+  //for temp,thyst, and tox, Bits D[15:0] contains the temperature data, with the LSB = 0.00390625 C and the MSB=sign bit
+};
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//creates software I2C port for Espruino devices
+function MAX30205(i2c) {
+    this.i2c = i2c;
+    this.ad = C.I2C_ADDR;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//basic function to read 8 bits from MAX30205 from a specified register via i2c
+MAX30205.prototype.read8 = function(reg) {
+    this.i2c.writeTo(this.ad, reg);
+    return this.i2c.readFrom(this.ad,1);
+};
+
+//basic function to write 8 bits to a specified register in the MAX30205 via i2c
+MAX30205.prototype.write8 = function(reg, value) {
+    this.i2c.writeTo(this.ad, reg, value);
+};
+
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Functions for MAX30205 temperature reading
@@ -16,8 +59,10 @@ MAX30205.prototype.shutdown = function(){ //shutdown with one-shot disabled 0 0 
 
 MAX30205.prototype.getTemperature = function(temperature, unit){
   
-  temperature = this.read8(C.REG_TEMPERATURE)[0];
-
+  let raw_temperature = 0;
+  raw_temperature = this.read8(C.REG_TEMPERATURE);
+  temperature = ~(raw_temperature);
+  
   if(unit == 0){ 
     temperature = temperature;
   }else{
